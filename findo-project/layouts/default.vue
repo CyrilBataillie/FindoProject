23cd97f4d8e07ae51e8c4ebc23d8542fc57827c3
Cyril Bataillie <cyril.bataillie@outlook.com>
2019-02-28 10:30:03 +0100

    Menu toggle

--- findo-project/layouts/default.vue
+++ findo-project/layouts/default.vue
@@ -1,12 +1,29 @@
 <template>
     <div class="layout">
-        <div class="layout-header">Findo</div>
+        <div class="layout-header">
+          <div class="layout-header-menu-handle" @click="toggleMenu"><icon name="bars"/></div>
+        </div>
         <div class="layout-menu" v-show="menuOpened">Menu content...</div>
         <div class="layout-content">
             <nuxt/>
         </div>
     </div>
 </template>
+
+<script>
+  export default {
+    data() {
+          return {
+              menuOpened: true,
+          }
+      },
+    methods: {
+    toggleMenu() {
+        this.menuOpened = !this.menuOpened
+    }
+}
+  }
+</script>
 
 <style>
   .layout-header {
@@ -24,12 +41,3 @@
   }
 </style>
 
-<script>
-  export default {
-    data() {
-          return {
-              menuOpened: false,
-          }
-      }
-  }
-</script>
