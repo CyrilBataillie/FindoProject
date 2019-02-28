bf357514f403d31e1c9d008ac7f51018af13607e
Cyril Bataillie <cyril.bataillie@outlook.com>
2019-02-28 10:21:13 +0100

    Layout Structure

--- findo-project/layouts/default.vue
+++ findo-project/layouts/default.vue
@@ -1,41 +1,35 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+    <div class="layout">
+        <div class="layout-header">Findo</div>
+        <div class="layout-menu">Menu content...</div>
+        <div class="layout-content">
+            <nuxt/>
+        </div>
+    </div>
 </template>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
+  .layout-header {
+      background-color: #DDD;
+      position: fixed; right: 0; top: 0; left: 0; height: 64px;
+  }
+  .layout-content {
+      background-color: #F7F7F7;
+      position: fixed; right: 0; bottom: 0; left: 0; top: 64px;
+  }
+  .layout-content > section { padding: 20px; }
+  .layout-menu {
+      background-color: #FFF; z-index: 10;
+      position: fixed; right: 0; bottom: 0; left: 0; top: 64px;
+  }
 </style>
+
+<script>
+  export default {
+    data() {
+          return {
+              menuOpened: false,
+          }
+      }
+  }
+</script>
