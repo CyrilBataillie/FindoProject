f8187638588e7aae5c5d52f6218484cc6ed52dbf
Cyril Bataillie <cyril.bataillie@outlook.com>
2019-02-28 10:35:10 +0100

    pretify menu

--- findo-project/layouts/default.vue
+++ findo-project/layouts/default.vue
@@ -1,43 +1,73 @@
 <template>
-    <div class="layout">
-        <div class="layout-header">
-          <div class="layout-header-menu-handle" @click="toggleMenu"><icon name="bars"/></div>
-        </div>
-        <div class="layout-menu" v-show="menuOpened">Menu content...</div>
-        <div class="layout-content">
-            <nuxt/>
-        </div>
+  <div class="layout">
+    <div class="layout-header">
+      <div class="layout-header-menu-handle" @click="toggleMenu">
+        <icon v-if="menuOpened" name="times" />
+        <icon v-else name="bars" />
+      </div>
     </div>
+    <transition name="slide-left">
+      <div class="layout-menu" v-show="menuOpened">Menu content...</div>
+    </transition>
+    <div class="layout-content">
+      <nuxt/>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     data() {
-          return {
-              menuOpened: true,
-          }
-      },
+      return {
+        menuOpened: true,
+      }
+    },
     methods: {
-    toggleMenu() {
+      toggleMenu() {
         this.menuOpened = !this.menuOpened
+      }
     }
-}
   }
 </script>
 
 <style>
   .layout-header {
-      background-color: #DDD;
-      position: fixed; right: 0; top: 0; left: 0; height: 64px;
+    background-color: #DDD;
+    position: fixed;
+    right: 0;
+    top: 0;
+    left: 0;
+    height: 64px;
   }
+  
   .layout-content {
-      background-color: #F7F7F7;
-      position: fixed; right: 0; bottom: 0; left: 0; top: 64px;
+    background-color: #F7F7F7;
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    top: 64px;
   }
-  .layout-content > section { padding: 20px; }
+  
+  .layout-content>section {
+    padding: 20px;
+  }
+  
   .layout-menu {
-      background-color: #FFF; z-index: 10;
-      position: fixed; right: 0; bottom: 0; left: 0; top: 64px;
+    background-color: #FFF;
+    z-index: 10;
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    top: 64px;
+  }
+  
+  .layout-header-menu-handle {
+    width: 64px;
+    height: 64px;
+    line-height: 64px;
+    text-align: center;
   }
 </style>
 
